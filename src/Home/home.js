fa2bba4edc02abf565048ffe1dd2f0875aaff90b
aka0039 <yumeno.13579@gmail.com>
2023-04-08 07:23:07 -0700

    create set-ui

---
+++ src/Home/home.js
@@ -0,0 +1,60 @@
+import { StyleSheet, View, Text, Alert, Pressable } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+
+export default function Home() {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Take it!!!</Text>
+      <Pressable onPress={() => navigation.navigate("Set")}>
+        <View style={styles.cardBoxStart}>
+          <Text style={styles.startText}>Start</Text>
+        </View>
+      </Pressable>
+      <Pressable onPress={() => Alert.alert("Score page")}>
+        <View style={styles.cardBoxSetting}>
+          <Text style={styles.settingText}>Score</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  titleBox: {
+    height: 100,
+  },
+  titleText: {
+    paddingVertical: 100,
+    fontSize: 36,
+    textAlign: "center",
+  },
+  cardBoxStart: {
+    borderRadius: 10,
+    marginVertical: 10,
+    marginHorizontal: 90,
+    height: 120,
+    backgroundColor: "#5DB075",
+    paddingBottom: 30,
+  },
+  startText: {
+    paddingTop: 30,
+    fontSize: 36,
+    textAlign: "center",
+  },
+  cardBoxSetting: {
+    borderRadius: 10,
+    marginVertical: 50,
+    marginHorizontal: 90,
+    height: 120,
+    backgroundColor: "#5DB075",
+  },
+  settingText: {
+    paddingTop: 30,
+    fontSize: 36,
+    textAlign: "center",
+  },
+});
