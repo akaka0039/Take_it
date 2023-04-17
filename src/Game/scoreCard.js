eb713c7440f9e354f6187626c42551bb701cfaf1
aka0039 <yumeno.13579@gmail.com>
2023-04-17 14:55:33 -0700

    add finding loser function

--- src/Game/scoreCard.js
+++ src/Game/scoreCard.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, Pressable, Alert } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import React from "react";
 import { Props } from "./Dummydata";
 
